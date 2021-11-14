aa6d2c4ff654fcb4eea90959444f02da9d5f5c22
Ujjwal Garg <ujjwalgarg.252@gmail.com>
2021-11-15 00:09:50 +0530

    Made create poll and participants page

--- src/components/CreatePoll/CreatePoll.jsx
+++ src/components/CreatePoll/CreatePoll.jsx
@@ -1,30 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { Socket } from "socket.io";
 import "./CreatePoll.scss";
 
 const CreatePoll = ({ socket, roomName }) => {
+  const [numOfOption, setNumOfOption] = useState(0);
+  const [pollingQuestion, setPollingQuestion] = useState();
+  const [options, setOptions] = useState({});
+
   const sendPoll = () => {
+    console.log("ujjwal", options);
     socket.emit("insertPoll", roomName, {
-      text: "Polling Question",
+      text: pollingQuestion,
       total_votes: 0,
-      options: [
-        { option: "option A", votes: [] },
-        { option: "option B", votes: [] },
-      ],
+      // options: [
+      //   { option: "option A", votes: [] },
+      //   { option: "option B", votes: [] },
+      // ],
+      options,
       createdBy: socket.id,
       createdAt: Date.now(),
     });
   };
+
+  const handleOption = async (e, i, selectedOption) => {
+    let option = { ...options };
+    option[i] = selectedOption;
+    await setOptions(Object.values(option));
+  };
+
   return (
     <div className="create-poll">
-      create-poll
-      <button
-        onClick={() => {
-          sendPoll();
-        }}
-      >
-        Send Poll
-      </button>
+      <div className="heading">Start Poll</div>
+      <div>
+        <label>Your Question</label>
+        <textarea
+          className="form-control form-control-alternative text-dark"
+          placeholder={`Enter your question`}
+          rows="3"
+          value={pollingQuestion}
+          onChange={(e) => setPollingQuestion(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Number of options </label>
+        <input
+          className="form-control form-control-alternative"
+          type="number"
+          placeholder="Enter number of options"
+          min={0}
+          onChange={(e) => {
+            setNumOfOption(parseInt(e.currentTarget.value, 10));
+          }}
+        ></input>
+      </div>
+      <div>
+        {!!numOfOption &&
+          [...Array(numOfOption)].map((el, index) => (
+            <>
+              <label>Option {index + 1}</label>
+              <textarea
+                id={index + 1}
+                key={index}
+                className="form-control form-control-alternative text-dark"
+                placeholder={`Enter option ${index + 1}`}
+                rows="2"
+                onChange={(e) =>
+                  handleOption(e, index, {
+                    option: e.target.value,
+                    votes: [],
+                  })
+                }
+              />
+            </>
+          ))}
+      </div>
+      {numOfOption > 0 && (
+        <button
+          className="accept"
+          onClick={() => {
+            sendPoll();
+          }}
+        >
+          Create
+        </button>
+      )}
     </div>
   );
 };
--- src/components/Participants/Participants.jsx
+++ src/components/Participants/Participants.jsx
@@ -5,7 +5,33 @@ import "./Participants.scss";
 const Participants = ({ participants }) => {
   console.log(participants);
   return (
-    <div className="participants">
+    <>
+      <div class="list-wrapper">
+        <h4>participants</h4>
+        <ul class="list">
+          {participants.map((participant, index) => {
+            return (
+              <li class="list-item" key={index}>
+                <div>
+                  <img
+                    src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg"
+                    class="list-item-image"
+                  />
+                </div>
+                <div class="list-item-content">
+                  <h4>
+                    {" "}
+                    {participant.displayName}{" "}
+                    {participant.user_id === socket.id ? ` (You)` : ``}
+                  </h4>
+                  <p>@hk-skit</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {/* <div className="participants">
       <h4>participants</h4>
       <ul>
         {participants.map((participant, index) => {
@@ -17,7 +43,8 @@ const Participants = ({ participants }) => {
           );
         })}
       </ul>
-    </div>
+    </div> */}
+    </>
   );
 };
 
